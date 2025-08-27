4b9541639dbc1d441a513ce51e82854aff288e99
Rajeev Tapadia <rtapadia730@gmail.com>
2025-08-27 17:04:13 +0530

    Destroy Pixels when they form a layer
    How destruction works?
    Flood fill algorithm with DFS is used to find layers that touch both the
    left and right screen boundries.
    
    TODO:
    show the destruction as a fade animation
    
    Signed-off-by: Rajeev Tapadia <rtapadia730@gmail.com>

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -284,8 +285,20 @@ void perform_dragging() {
     draw_ghost(grid_pos_to_vector2(drag_state.position_offset));
 }
 
+bool is_panel_empty() {
+    bool is_active = false;
+    ;
+    for (int i = 0; i < OPTIONS_COUNT; i++) {
+        is_active = options_panel[i].active;
+    }
+    return !is_active;
+}
+
 // TODO: select figures and colors by random choice
 void generate_options() {
+    if (!is_panel_empty() || drag_state.dragging)
+        return;
+
     int options_panel_col_width = OPTIONS_COLS / OPTIONS_COUNT;
 
     for (int i = 0; i < OPTIONS_COUNT; i++) {
@@ -313,6 +326,92 @@ void render_options_panel() {
     }
 }
 
+bool can_move() {
+    for (int row = GRID_ROWS - 2; row >= 0; row--) {
+        for (int col = 1; col < GRID_COLS - 1; col++) {
+            if (board[row][col].active) {
+
+                if (!board[row + 1][col].active)
+                    return true;
+                if (!board[row + 1][col - 1].active || !board[row + 1][col + 1].active)
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+
+void dfs(int row, int col, bool visited[GRID_ROWS][GRID_COLS], Color color, bool *touches_left, bool *touches_right) {
+    if (row < 0 || row >= GRID_ROWS) {
+        return;
+    }
+    if (col < 0 || col >= GRID_COLS) {
+        return;
+    }
+    if (!board[row][col].active) {
+        return;
+    }
+    if (visited[row][col]) {
+        return;
+    }
+    if (memcmp(&board[row][col].color, &color, sizeof(color)) == 0) {
+        visited[row][col] = true;
+        if (col == 0) {
+            *touches_left = true;
+        }
+        if (col == GRID_COLS - 1) {
+            *touches_right = true;
+        }
+        dfs(row + 1, col, visited, color, touches_left, touches_right);
+        dfs(row - 1, col, visited, color, touches_left, touches_right);
+        dfs(row, col - 1, visited, color, touches_left, touches_right);
+        dfs(row, col - 1, visited, color, touches_left, touches_right);
+    }
+}
+
+void remove_visited(bool visited[GRID_ROWS][GRID_COLS]) {
+    for (int row = GRID_ROWS - 1; row >= 0; row--) {
+        for (int col = 0; col < GRID_COLS; col++) {
+            if (visited[row][col] == 1) {
+                board[row][col] = (Pixel){0};
+            }
+        }
+    }
+}
+
+void flood_fill() {
+    if (can_move()) {
+        return;
+    }
+
+    for (int row = GRID_ROWS - 1; row >= 0; row--) {
+        // check from left most Pixel
+        {
+            bool touches_left = false;
+            bool touches_right = false;
+            Color color = board[row][0].color;
+            bool visited[GRID_ROWS][GRID_COLS] = {0};
+            dfs(row, 0, visited, color, &touches_left, &touches_right);
+
+            if (touches_left && touches_right) {
+                remove_visited(visited);
+            }
+        }
+
+        // check from right most Pixel
+        {
+            bool touches_left = false;
+            bool touches_right = false;
+            bool visited[GRID_ROWS][GRID_COLS] = {0};
+            Color color = board[row][GRID_COLS - 1].color;
+            dfs(row, GRID_COLS - 1, visited, color, &touches_left, &touches_right);
+            if (touches_left && touches_right) {
+                remove_visited(visited);
+            }
+        }
+    }
+}
+
 int main(void) {
     printf("gooning simulator\n");
     const int pixel_width = PIXEL_SIZE;
@@ -327,12 +426,13 @@ int main(void) {
 
     create_square_block(square_pos, GREEN);
     // create_L_block(square_pos, GREEN);
-    generate_options();
 
     float timer = 0.0f;
     float delay = 0.005f;
 
     while (!WindowShouldClose()) {
+        generate_options();
+
         float dt = GetFrameTime();
         timer += dt;
 
@@ -361,6 +461,10 @@ int main(void) {
 
         detect_dragging();
         perform_dragging();
+
+        if (!drag_state.dragging) {
+            flood_fill();
+        }
 
         EndDrawing();
     }
